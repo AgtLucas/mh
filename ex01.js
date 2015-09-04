72a975c13bacb345276f0e1c043cbe08929e1013
Lucas <lucas@agtlucas.com>
2015-09-04 08:12:29 -0300

    Update ex01.js

--- ex01.js
+++ ex01.js
@@ -5,3 +5,13 @@ server.connection({
   host: 'localhost',
   port: Number(process.argv[2] || 8000)
 });
+
+server.route({
+  path: '/',
+  method: 'GET',
+  handler: function(request, reply) {
+    reply('Hello hapi');
+  }
+});
+
+server.start(function() {});
