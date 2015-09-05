1db4c2bdf687359481eb62363d8c8b6718122bd3
Lucas <lucas@agtlucas.com>
2015-09-05 12:08:27 -0300

    Update ex02.js

---
+++ ex02.js
@@ -0,0 +1,11 @@
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+server.connection({
+  host: 'localhost',
+  port: Number(process.argv[2] || 8080)
+});
+
+function handler(request, reply) {
+  reply('Hello ' + request.params.name);
+}
