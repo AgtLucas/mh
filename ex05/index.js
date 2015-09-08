790b69b6caa0db62a222b299f299964aa7763911
Lucas <lucas@agtlucas.com>
2015-09-08 08:15:07 -0300

    Fix typo on index.js

---
+++ ex05/index.js
@@ -0,0 +1,30 @@
+var Hapi = require('hapi');
+var Vision = require('vision');
+var path = require('path');
+var server = new Hapi.Server();
+
+server.register(Vision, function(err) {
+  if (err) throw err;
+});
+
+server.connection({
+  host: 'localhost',
+  port: Number(process.argv[2] || 8080)
+});
+
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  path: path.join(__dirname, 'templates')
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: {
+    view: 'index.html'
+  }
+});
+
+server.start(function() {});
