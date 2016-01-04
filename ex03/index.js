fc86909875128c102096bc0569037dad7fca17ac
Lucas <AgtLucas@users.noreply.github.com>
2016-01-04 00:32:36 -0200

    Update index.js

--- ex03/index.js
+++ ex03/index.js
@@ -1,16 +1,16 @@
-var Hapi = require('hapi');
-var Inert = require('inert');
-var path = require('path');
-var server = new Hapi.Server();
+var Hapi = require('hapi')
+var Inert = require('inert')
+var path = require('path')
+var server = new Hapi.Server()
 
 server.register(Inert, function(err) {
-  if (err) throw err;
-});
+  if (err) throw err
+})
 
 server.connection({
   host: 'localhost',
   port: Number(process.argv[2] || 8080)
-});
+})
 
 server.route({
   method: 'GET',
@@ -18,6 +18,6 @@ server.route({
   handler: {
     file: path.join(__dirname, 'index.html')
   }
-});
+})
 
-server.start(function() {});
+server.start(function() {})
