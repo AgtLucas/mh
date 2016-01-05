6afbc000dcf610bedb1f487030d88cea89a778e2
Lucas <AgtLucas@users.noreply.github.com>
2016-01-05 07:31:44 -0200

    Update index.js

--- ex05/index.js
+++ ex05/index.js
@@ -1,23 +1,23 @@
-var Hapi = require('hapi');
-var Vision = require('vision');
-var path = require('path');
-var server = new Hapi.Server();
+var Hapi = require('hapi')
+var Vision = require('vision')
+var path = require('path')
+var server = new Hapi.Server()
 
 server.register(Vision, function(err) {
-  if (err) throw err;
-});
+  if (err) throw err
+})
 
 server.connection({
   host: 'localhost',
   port: Number(process.argv[2] || 8080)
-});
+})
 
 server.views({
   engines: {
     html: require('handlebars')
   },
   path: path.join(__dirname, 'templates')
-});
+})
 
 server.route({
   method: 'GET',
@@ -25,6 +25,6 @@ server.route({
   handler: {
     view: 'index.html'
   }
-});
+})
 
-server.start(function() {});
+server.start(function() {})
